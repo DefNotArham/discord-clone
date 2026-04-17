2a97c0f0a3e17bb7339ea9e33f37b67d55f66a33
DefNotArham <arhamkabir231@gmail.com>
2026-04-17 04:14:03 -0400

    added username in backend

---
+++ backend/controllers/changeUsername.controller.js
@@ -0,0 +1,58 @@
+import User from "../model/user.model.js";
+
+const changeUsernameController = async (req, res) => {
+  const { newUsername } = req.body;
+  try {
+    if (!newUsername)
+      return res.status(400).json({
+        success: false,
+        message: "Please enter a username ",
+      });
+
+    const user = await User.findById(req.userId).select("-password");
+
+    if (!user)
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+
+    if (newUsername.includes(" "))
+      return res.status(400).json({
+        success: false,
+        message: "Username cannot contain spaces",
+        typeError: "username",
+      });
+
+    if (newUsername.length < 3 || newUsername.length > 20)
+      return res.status(400).json({
+        success: false,
+        message: "Username must be 3–20 characters long",
+        typeError: "username",
+      });
+
+    const existUsername = await User.findOne({ username });
+    if (existUsername)
+      return res.status(400).json({
+        success: false,
+        message:
+          "Username is unavailable. Try adding numbers, letters, underscores _ , or periods.",
+        typeError: "username",
+      });
+
+    user.username = newUsername;
+    await user.save();
+    res.status(200).json({
+      success: true,
+      message: "Username changed successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+export default changeUsernameController;
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
 import changeStatusController from "../controllers/changeStatus.controller.js";
 import editDisplayNameController from "../controllers/editDisplayName.controller.js";
+import changeUsernameController from "../controllers/changeUsername.controller.js";
 
 const router = express.Router();
 
 router.post("/change-status", verifyToken, changeStatusController);
 router.post("/change-displayName", verifyToken, editDisplayNameController);
+router.post("/change-username", verifyToken, changeUsernameController);
 
 export default router;
