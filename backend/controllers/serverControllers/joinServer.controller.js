d576b0e3d9d52e66a8cba1e166e36c4a7d0bd3d1
DefNotArham <arhamkabir231@gmail.com>
2026-04-20 21:18:21 -0400

    created join server backend

---
+++ backend/controllers/serverControllers/joinServer.controller.js
@@ -0,0 +1,52 @@
+import User from "../../model/user.model.js";
+import Server from "../../model/server.model.js";
+
+const joinServerController = async (req, res) => {
+  const { inviteCode } = req.body;
+  try {
+    const user = await User.findById(req.userId).select("-password");
+
+    if (!user)
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+
+    const server = await Server.findOne({
+      inviteCode,
+    });
+
+    if (!server)
+      return res.status(400).json({
+        success: false,
+        message: "Server not found",
+      });
+
+    const alreadyMember = server.members.includes(req.userId);
+
+    if (alreadyMember)
+      return res.status(400).json({
+        success: false,
+        message: "You are already in this server",
+      });
+
+    server.members.push(req.userId);
+    await server.save();
+
+    user.servers.push(server._id);
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Joined server successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+export default joinServerController;
--- backend/routes/server.routes.js
+++ backend/routes/server.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import createServerController from "../controllers/serverControllers/createServer.controller.js";
 import verifyToken from "../middleware/verifyToken.js";
 import loadServerController from "../controllers/serverControllers/loadServer.controller.js";
+import joinServerController from "../controllers/serverControllers/joinServer.controller.js";
 
 const router = express.Router();
 
 router.post("/create-server", verifyToken, createServerController);
 router.get("/load-server/:serverId", verifyToken, loadServerController);
+router.post("/join-server", verifyToken, joinServerController);
 
 export default router;
