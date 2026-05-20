6ffc779faf085078e6caaa2a55151b75e891d5e3
DefNotArham <arhamkabir231@gmail.com>
2026-05-20 14:41:08 -0400

    added remove frined route

--- backend/routes/friend.routes.js
+++ backend/routes/friend.routes.js
@@ -7,6 +7,7 @@ import addFriendController from "../controllers/friendController/addFriend.contr
 import declineAddFriendController from "../controllers/friendController/declineAddfriend.controller.js";
 import getFriendRequestsController from "../controllers/friendController/getFriendReqests.controller.js";
 import loadFriendsController from "../controllers/friendController/loadFriends.controller.js";
+import removeFriendController from "../controllers/friendController/removeFriend.controller.js";
 
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.post("/add-friend", verifyToken, addFriendController);
 router.post("/decline-friend", verifyToken, declineAddFriendController);
 router.get("/get-friendRequests", verifyToken, getFriendRequestsController);
 router.get("/get-friends", verifyToken, loadFriendsController);
+router.delete("/remove-friend", verifyToken, removeFriendController);
 
 export default router;
